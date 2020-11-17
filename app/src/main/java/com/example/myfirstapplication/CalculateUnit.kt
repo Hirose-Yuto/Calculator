afe2604a26db852a305707bb11e84d7d8f388c52
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2020-11-17 19:02:20 +0900

    refactor

--- app/src/main/java/com/example/myfirstapplication/CalculateUnit.kt
+++ app/src/main/java/com/example/myfirstapplication/CalculateUnit.kt
@@ -17,6 +17,7 @@ public class CalculateUnit {
         Left, Operator, Right
     }
 
+    // 1/xや三角関数などの一つのみを引数とするような演算(その場で実行)
     fun singleOperator(operator: SingleOperator, option: Int = 0) {
         var tmpVal = when(calcState) {
             CalcState.Left -> leftVal
@@ -34,10 +35,42 @@ public class CalculateUnit {
         }
     }
 
+    // x^yのような2つのものを引数として取るような演算(イコールキーで実行)
+    fun setDoubleOperator(operator: DoubleOperator) {
+        this.operator = operator
+        calcState = CalcState.Operator
+    }
+
+    /*
+     * イコールが押されたときの処理
+     * 左入力してるときは何もしない
+     * 演算子入力中は左の値を右の値として扱う
+     */
+    fun calculate() {
+        when(calcState) {
+            CalcState.Left -> {return}
+            CalcState.Operator -> {rightVal = leftVal}
+            CalcState.Right -> {}
+        }
+        leftVal = when(operator) {
+            DoubleOperator.Nothing -> leftVal
+            DoubleOperator.Plus -> plus()
+            DoubleOperator.Minus -> minus()
+            DoubleOperator.Multiply -> multiply()
+            DoubleOperator.Divide -> divide()
+        }
+        calcState = CalcState.Left
+        operator = DoubleOperator.Nothing
+        rightVal = 0.0
+    }
+
+    /* Single Operator */
+    // 数字キーによる入力
     private fun insert(value: Double, num: Int): Double {
         return value * 10 + num
     }
 
+    // 1/x
     private fun frac(value: Double): Double {
         var returnValue = 0.0
         try {
@@ -48,47 +81,23 @@ public class CalculateUnit {
         return returnValue
     }
 
-    fun calculate() {
-        when(calcState) {
-            CalcState.Left -> {}
-            CalcState.Operator -> {
-                rightVal = leftVal
-                operate()
-                calcState = CalcState.Left
-                operator = DoubleOperator.Nothing
-                rightVal = 0.0
-            }
-            CalcState.Right -> {
-                operate()
-                calcState = CalcState.Left
-                operator = DoubleOperator.Nothing
-                rightVal = 0.0
-            }
-        }
-    }
-
-    private fun operate() {
-        leftVal = when(operator) {
-            DoubleOperator.Nothing -> 0.0
-            DoubleOperator.Plus -> plus()
-            DoubleOperator.Minus -> minus()
-            DoubleOperator.Multiply -> multiply()
-            DoubleOperator.Divide -> divide()
-        }
-    }
-
+    /* Double Operator*/
+    // 加算
     private fun plus(): Double {
         return  leftVal + rightVal
     }
 
+    // 減算
     private fun minus(): Double {
         return leftVal - rightVal
     }
 
+    // 乗算
     private fun multiply(): Double {
         return leftVal * rightVal
     }
 
+    // 除算 例外が発生したらACして0を返す
     private fun divide(): Double {
         var returnValue = 0.0
         try {
@@ -99,11 +108,7 @@ public class CalculateUnit {
         return returnValue
     }
 
-    fun setDoubleOperator(operator: DoubleOperator) {
-        this.operator = operator
-        calcState = CalcState.Operator
-    }
-
+    // Cキー
     fun clear() {
         when(calcState) {
             CalcState.Left -> {
@@ -118,6 +123,7 @@ public class CalculateUnit {
         }
     }
 
+    // ACキー
     fun allClear() {
         leftVal = 0.0
         operator = DoubleOperator.Nothing
@@ -125,10 +131,12 @@ public class CalculateUnit {
         calcState = CalcState.Left
     }
 
+    // どの入力をしているか取得
     fun getCalcState(): CalcState {
         return calcState
     }
 
+    // 表示する値を取得
     fun getDisplayValue(): Double {
         when(calcState) {
             CalcState.Left -> {
